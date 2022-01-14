9390ed8747026cbe5cb2b42d8db4dac769761f7b
Panagiotis Bellias <belliaspanagiotis@gmail.com>
2022-01-14 15:27:13 +0200

    admin form customization

--- dynamic_application_system/applications/admin.py
+++ dynamic_application_system/applications/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from . import models
 
 # Panagiotis Bellias
-admin.site.register(models.Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    #fields = ['pub_date', 'headline', 'content']
+    fieldsets = [
+        ('Content Information', {'fields':['headline', 'content']}),
+        ('Date Information', {'fields':['pub_date']}),
+    ]
+
+admin.site.register(models.Application, ApplicationAdmin)
+
+
 admin.site.register(models.Carrier)
 admin.site.register(models.Citizen)
 admin.site.register(models.Manager)
